a62309561625951d5464e5f7197e91e42f809032
lakefu <leavethewindoff@163.com>
2024-03-05 02:28:39 +0800

    .

--- service.go
+++ service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync/atomic"
 )
 
 type QueueMsg struct {
@@ -10,58 +11,87 @@ type QueueMsg struct {
 	call   func()
 }
 
-type Service struct {
+type Service interface {
+	Enqueue(func())
+	Run()
+	Await(func())
+	Quit()
+}
+
+type BaseService struct {
 	queue    chan *QueueMsg
 	suspend  chan error
 	quit     chan struct{}
 	quitFlag bool
-	pool     *CoPool
-	runCo    *Coroutine
 }
 
-func (s *Service) Await(call func()) {
-	if s.pool != nil {
-		co := s.runCo
-		s.suspend <- nil
-		co.state = Suspended
-		call()
-		s.queue <- &QueueMsg{
-			wakeup: func() {
-				s.runCo = co
-				err := co.Resume(nil, s.suspend)
-				s.runCo = nil
-				if err != nil {
-					log.Printf("handle msg call err, %v\n", err)
-				}
-			},
+func (s BaseService) Enqueue(f func()) {
+	s.queue <- &QueueMsg{call: f}
+}
+
+func (s *BaseService) Quit() {
+	s.quitFlag = true
+	close(s.quit)
+}
+
+type GoService struct {
+	BaseService
+	goSeq uint64
+}
+
+func (s *GoService) Await(call func()) {
+	curSeq := atomic.LoadUint64(&s.goSeq)
+	s.suspend <- nil
+	call()
+	//TODO: use sync.Pool??
+	c := make(chan struct{})
+	s.queue <- &QueueMsg{
+		wakeup: func() {
+			atomic.StoreUint64(&s.goSeq, curSeq)
+			c <- struct{}{}
+		},
+	}
+	<-c
+}
+
+func (s *GoService) Run() {
+	for !s.quitFlag {
+		select {
+		case msg := <-s.queue:
+			if msg.wakeup != nil {
+				msg.wakeup()
+			} else {
+				go s.handleMsg(msg.call)
+			}
+			<-s.suspend
+		case <-s.quit:
+			break
 		}
-		<-co.waitIn
-	} else {
-		s.suspend <- nil
-		func() {
-			defer func() {
-				if e := recover(); e != nil {
-					log.Printf("go running err: %v\n", e)
-				}
-			}()
-			call()
-		}()
-		c := make(chan struct{})
-		s.queue <- &QueueMsg{
-			wakeup: func() {
-				c <- struct{}{}
-			},
+	}
+
+	for {
+		select {
+		case msg := <-s.queue:
+			if msg.wakeup != nil {
+				msg.wakeup()
+			} else {
+				go s.handleMsg(msg.call)
+			}
+			<-s.suspend
+		default:
+			return
 		}
-		<-c
 	}
 }
 
-func (s *Service) handleMsg(call func()) {
+func (s *GoService) handleMsg(call func()) {
+	curSeq := atomic.AddUint64(&s.goSeq, 1)
 	defer func() {
 		if e := recover(); e != nil {
 			log.Printf("go running err, %v\n", e)
-			//TODO: 只有框架正在处理的go发生异常需要唤醒suspend
-			s.suspend <- fmt.Errorf("%v", e)
+			if curSeq != atomic.LoadUint64(&s.goSeq) {
+				s.suspend <- fmt.Errorf("%v", e)
+			}
 		} else {
 			s.suspend <- nil
 		}
@@ -69,35 +99,44 @@ func (s *Service) handleMsg(call func()) {
 	call()
 }
 
-func (s *Service) Enqueue(call func()) {
-	s.queue <- &QueueMsg{
-		call: call,
-	}
+type CoService struct {
+	BaseService
+	pool  *CoPool
+	runCo *Coroutine
 }
 
-func (s *Service) Run() {
+func (s *CoService) Await(call func()) {
+	co := s.runCo
+	s.suspend <- nil
+	co.state = Suspended
+	call()
+	s.queue <- &QueueMsg{
+		wakeup: func() {
+			s.runCo = co
+			err := co.Resume(nil, s.suspend)
+			s.runCo = nil
+			if err != nil {
+				log.Printf("handle msg call err, %v\n", err)
+			}
+		},
+	}
+	<-co.waitIn
+}
+
+func (s *CoService) Run() {
 	for !s.quitFlag {
 		select {
 		case msg := <-s.queue:
-			if s.pool != nil {
-				if msg.wakeup != nil {
-					msg.wakeup()
-				} else {
-					co := s.pool.Get()
-					s.runCo = co
-					err := co.Resume(msg.call, s.suspend)
-					s.runCo = nil
-					if err != nil {
-						log.Printf("handle msg call err, %v\n", err)
-					}
-				}
+			if msg.wakeup != nil {
+				msg.wakeup()
 			} else {
-				if msg.wakeup != nil {
-					msg.wakeup()
-				} else {
-					go s.handleMsg(msg.call)
+				co := s.pool.Get()
+				s.runCo = co
+				err := co.Resume(msg.call, s.suspend)
+				s.runCo = nil
+				if err != nil {
+					log.Printf("handle msg call err, %v\n", err)
 				}
-				<-s.suspend
 			}
 		case <-s.quit:
 			break
@@ -107,25 +146,16 @@ func (s *Service) Run() {
 	for {
 		select {
 		case msg := <-s.queue:
-			if s.pool != nil {
-				if msg.wakeup != nil {
-					msg.wakeup()
-				} else {
-					co := s.pool.Get()
-					s.runCo = co
-					err := co.Resume(msg.call, s.suspend)
-					s.runCo = nil
-					if err != nil {
-						log.Printf("handle msg call err, %v\n", err)
-					}
-				}
+			if msg.wakeup != nil {
+				msg.wakeup()
 			} else {
-				if msg.wakeup != nil {
-					msg.wakeup()
-				} else {
-					go s.handleMsg(msg.call)
+				co := s.pool.Get()
+				s.runCo = co
+				err := co.Resume(msg.call, s.suspend)
+				s.runCo = nil
+				if err != nil {
+					log.Printf("handle msg call err, %v\n", err)
 				}
-				<-s.suspend
 			}
 		default:
 			return
@@ -133,19 +163,25 @@ func (s *Service) Run() {
 	}
 }
 
-func (s *Service) Quit() {
-	s.quitFlag = true
-	close(s.quit)
-}
-
-func NewService(qsize, psize int) *Service {
-	s := &Service{
-		queue:   make(chan *QueueMsg, qsize),
-		suspend: make(chan error, 1),
-		quit:    make(chan struct{}),
-	}
+func NewService(qsize, psize int) (s Service) {
 	if psize > 0 {
-		s.pool = NewCoPool(psize)
+		cs := &CoService{
+			BaseService: BaseService{
+				queue:   make(chan *QueueMsg, qsize),
+				suspend: make(chan error, 1),
+				quit:    make(chan struct{}),
+			},
+		}
+		cs.pool = NewCoPool(psize)
+		s = cs
+	} else {
+		s = &GoService{
+			BaseService: BaseService{
+				queue:   make(chan *QueueMsg, qsize),
+				suspend: make(chan error, 1),
+				quit:    make(chan struct{}),
+			},
+		}
 	}
-	return s
+	return
 }
